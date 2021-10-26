70be98cb1f9a7823155cbe0a62ee75e06ec9fb32
abdulla <abdulla@lune.ae>
2021-10-27 01:32:02 +0400

    Version 1.1

--- wpolice.py
+++ wpolice.py
@@ -3,15 +3,13 @@ import os
 import platform
 os_type=platform.system().lower()
 def block(pathh,site):
-    path_to_hosts=pathh
-    with open(path_to_hosts,"a") as hsts:
+    with open(pathh,"a") as hsts:
         line="\n0.0.0.0       "+site
         hsts.write(line)
 def unblock(pathh,site):
-    path_to_hosts=os.path.join("C:",os.sep,"Windows","System32","Drivers","etc","hosts")
-    with open(path_to_hosts, "r") as f:
+    with open(pathh, "r") as f:
         lines = f.readlines()
-    with open(path_to_hosts, "w") as f:
+    with open(pathh, "w") as f:
         for line in lines:
             if site not in line.strip("\n") :
                 f.write(line)
@@ -22,6 +20,9 @@ def logic(pathh):
         block(pathh,site)
     elif(arg1=="-u"):
         unblock(pathh,site)
+    else:
+        print("Erorr: please use appropriate flags")
+        sys.exit(1)
 
 if(os_type=="windows"):
     def is_admin():
@@ -36,4 +37,4 @@ if(os_type=="windows"):
         ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
 else:
     path_to_hosts="/etc/hosts"
-    logic()
+    logic(path_to_hosts)
